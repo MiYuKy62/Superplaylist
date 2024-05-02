849425184e0ae62862f8848dd41737d1c1505cf7
MiYuKy62 <mat62142@gmail.com>
2024-05-02 09:58:25 +0200

    2

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,11 +60,12 @@ const hideDisque = () => {
 
 
 const getData = async () =>{
-    const req = await fetch("./assets/js/data.json");
+    const req = await fetch("http://localhost:3000/api/v1/musics");
     console.log(req);
     const dbMusic = await req.json();
     console.log("result ", dbMusic);
-    dbMusic.forEach((music) => {
+    data = dbMusic.result;
+    data.forEach((music) => {
         const textLengthClass = music.title.length > 15 ? "long-text" : "";
         playlist.innerHTML += `<li id="${music.id}"><h2>${music.title} </h2><div><small> ${music.artiste} </small> </div></li> ` 
     });
@@ -115,9 +116,10 @@ const getData = async () =>{
         
 }
 
-getData();
-
 lecteur.addEventListener("pause", () => disque.classList.add("pause"));
 lecteur.addEventListener("play", () => disque.classList.remove("pause"));
+
+getData();
+
 
 
